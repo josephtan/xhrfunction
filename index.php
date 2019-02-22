cd3b519efd469619328437285eb56ef6b514f201
josephtan <tantjernbit@gmail.com>
2019-02-22 13:26:40 +1100

    header updates

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    $allowedOrigins = array(
+        '(http(s)://)?(www\.)?https://reqres\.in'
+    );
+
+    if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+        foreach ($allowedOrigins as $allowedOrigin) {
+            if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+                header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+                header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+                header('Access-Control-Max-Age: 1000');
+                header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+                break;
+            }
+        }
+    }
 }
 
 // Access-Control headers are received during OPTIONS requests
@@ -13,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
     exit(0);
 }
