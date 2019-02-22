860e713b30ef1d222f8601a2b526fb69d01293ca
josephtan <tantjernbit@gmail.com>
2019-02-22 13:00:20 +1100

    update headers

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 $origin = $_SERVER['HTTP_ORIGIN'];
 $allowed_domains = [
-    'https://reqres.in/api/users',
-    'http://13.211.249.108/'
+    'https://reqres.in/api/users'
 ];
 
 if (in_array($origin, $allowed_domains)) {
     header('Access-Control-Allow-Origin: ' . $origin);
-    header('Access-Control-Allow-Methods: GET, POST');
-    header("Access-Control-Allow-Headers: X-Requested-With");
+    header('Access-Control-Allow-Methods: POST,GET,PUT,DELETE');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 }?>
 <!DOCTYPE html>
 <html lang="en">
