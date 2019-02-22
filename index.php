21ff7d3522bf9e5db658c85000fb3bf91b09c68d
josephtan <tantjernbit@gmail.com>
2019-02-22 11:41:34 +1100

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+ <?php
+ header("Access-Control-Allow-Origin: *");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<script type="application/javascript" src="js/main.js"></script>
+</body>
+</html>
