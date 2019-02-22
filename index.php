a0bcdfff85d8b6efe2c60c099c25a4ffb8f6b537
josephtan <tantjernbit@gmail.com>
2019-02-22 11:51:50 +1100

    change header permission from wildcard to fixed domains

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
+<?php
+$origin = $_SERVER['HTTP_ORIGIN'];
+$allowed_domains = [
+    'https://my-json-server.typicode.com/'
+];
+
+if (in_array($origin, $allowed_domains)) {
+    header('Access-Control-Allow-Origin: ' . $origin);
+}?>
 <!DOCTYPE html>
 <html lang="en">
-<?php
-header("Access-Control-Allow-Origin: *");?>
+
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
